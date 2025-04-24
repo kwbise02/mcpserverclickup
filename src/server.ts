8a87a969f598328aeb619a5daf181c055fd70ff3
kwbise02 <kwbise02@gmail.com>
2025-04-23 19:05:01 -0500

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { MCPServer } from "@/modelcontextprotocol/sdk/server.js";
+import { clickUpServices } from "./services/shared.js";
+
+export const server = new MCPServer();
+
+export async function configureServer() {
+  // Add ClickUp services to the server
+  for (const [name, service] of Object.entries(clickUpServices)) {
+    server.addTool(name, service);
+  }
+}
